9acd2d0f4b081408b78bc246d19210208bb1d675
Stacey Lewis <stacey.lws@googlemail.com>
2023-08-01 22:37:27 +0000

    Add javascript link and start xhr request

---
+++ assets/js/script.js
@@ -0,0 +1,13 @@
+// remember to do testing on this before anything
+function getData() {
+  let xhr = new XMLHttpRequest();
+
+  xhr.open("GET", "https://secure.geonames.org/countryInfoJSON?username=staceylewis");
+  xhr.send();
+
+  xhr.onreadystatechange = function () {
+    if (this.readyState == 4 && this.status === 200) {
+      console.log(JSON.parse(this.responseText));
+    }
+  }
+};
