af07cea539435d3c6b263cc359ead024a87f0702
Stacey Lewis <stacey.lws@googlemail.com>
2023-08-15 22:20:33 +0000

    Add:destination header to display chosen city name

--- assets/scripts/tests/script.test.js
+++ assets/scripts/tests/script.test.js
@@ -1,4 +1,17 @@
-const formValidationCheck = require("../script");
+/**
+ * @jest-environment jsdom
+ */
+
+const { getEnvironmentData } = require("worker_threads");
+const {formValidationCheck} = require("../script");
+
+beforeAll(() => {
+    let fs = require("fs");
+    let fileContents = fs.readFileSync("index.html", "utf-8");
+    document.open();
+    document.write(fileContents);
+    document.close();
+});
 
 describe("get data once form validated", () => {
     describe("check if form is valid on submit", () => {
@@ -7,3 +20,10 @@ describe("get data once form validated", () => {
         });
     });
 });
+
+// describe('array containing country names', () => {
+//     const expectedCountries = ['United Kingdom','Greece'];
+//     it('matches even if contains additional elements', () => {
+//         expect(['United Kingdom', 'Greece', 'Cyrpus']).toEqual(expect.arrayContaining(expectedCountries));
+//       }); 
+// });
